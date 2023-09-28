46f7c952275f37869bd8067d28ab682d8209c3b8
celenny <celennycs@gmail.com>
2023-09-28 19:35:16 -0300

    alura-cook page 1

---
+++ src/lib/components/IngredienteSelecionavel.svelte
@@ -0,0 +1,35 @@
+<!-- componente especializado -->
+<!-- A especialização de componentes trata-se de uma estratégia que permite encapsular
+melhor comportamentos específicos, além de organizar melhor os componentes. -->
+
+<script lang="ts">
+    import { createEventDispatcher } from "svelte";
+    import Tag from "./Tag.svelte";
+    const dispatch = createEventDispatcher<{
+        adicionarIngrediente: string,
+        removerIngrediente: string
+    }>();
+    export let ingrediente: string;
+    let selecionado = false;
+
+    function aoClicar() {
+        selecionado = !selecionado;
+
+        if (selecionado) {
+            dispatch('adicionarIngrediente', ingrediente);
+        } else {
+            dispatch('removerIngrediente', ingrediente);
+        }
+    }
+</script>
+
+<button on:click={aoClicar}>
+    <Tag ativa={selecionado}>{ingrediente}</Tag>
+</button>
+
+
+<style>
+    button {
+        cursor: pointer;
+    }
+</style>
---
+++ src/routes/receitas/+page.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+    <title>Alura Cook | Receitas</title>
+</svelte:head>
+
+<p>Aqui estão as receitas</p>
+
+<a href="/">voltar para a página principal</a>
